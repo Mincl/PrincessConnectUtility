f37c1a3057751c7f8d3fe9eb8bd39e96c836a383
mincl <junseo.lee@navercorp.com>
2019-04-09 11:10:35 +0900

    icon name과 data name의 mismatch 수정 data 파일 분리, 모바일 뷰포트 대응, native js -> jQuery

---
+++ data.js
@@ -0,0 +1,464 @@
+var data={
+"chara_1":{
+	id: "chara_1",
+	name: "노조미",
+	icon: "icons/노조미.png",
+	range: 160
+},
+"chara_2":{
+	id: "chara_2",
+	name: "타마키",
+	icon: "icons/타마키.png",
+	range: 215
+},
+"chara_3":{
+	id: "chara_3",
+	name: "모니카",
+	icon: "icons/모니카.png",
+	range: 410
+},
+"chara_4":{
+	id: "chara_4",
+	name: "미야코",
+	icon: "icons/미야코.png",
+	range: 125
+},
+"chara_5":{
+	id: "chara_5",
+	name: "시오리",
+	icon: "icons/시오리.png",
+	range: 710
+},
+"chara_6":{
+	id: "chara_6",
+	name: "리마",
+	icon: "icons/리마.png",
+	range: 105
+},
+"chara_7":{
+	id: "chara_7",
+	name: "쿠우카",
+	icon: "icons/쿠우카.png",
+	range: 130
+},
+"chara_8":{
+	id: "chara_8",
+	name: "쥰",
+	icon: "icons/쥰.png",
+	range: 135
+},
+"chara_9":{
+	id: "chara_9",
+	name: "카오리",
+	icon: "icons/카오리.png",
+	range: 145
+},
+"chara_10":{
+	id: "chara_10",
+	name: "신년 레이",
+	icon: "icons/레이(신).png",
+	range: 153
+},
+"chara_11":{
+	id: "chara_11",
+	name: "페코린느",
+	icon: "icons/페코린느.png",
+	range: 155
+},
+"chara_12":{
+	id: "chara_12",
+	name: "루카",
+	icon: "icons/루카.png",
+	range: 158
+},
+"chara_13":{
+	id: "chara_13",
+	name: "무이미",
+	icon: "icons/무이미.png",
+	range: 162
+},
+"chara_14":{
+	id: "chara_14",
+	name: "마코토",
+	icon: "icons/마코토.png",
+	range: 165
+},
+"chara_15":{
+	id: "chara_15",
+	name: "신년 히요리",
+	icon: "icons/히요리(신).png",
+	range: 170
+},
+"chara_16":{
+	id: "chara_16",
+	name: "아키노",
+	icon: "icons/아키노.png",
+	range: 180
+},
+"chara_17":{
+	id: "chara_17",
+	name: "마츠리",
+	icon: "icons/마츠리.png",
+	range: 185
+},
+"chara_18":{
+	id: "chara_18",
+	name: "발렌타인 에리코",
+	icon: "icons/에리코(발).png",
+	range: 187
+},
+"chara_19":{
+	id: "chara_19",
+	name: "크리스마스 아야네",
+	icon: "icons/아야네(성).png",
+	range: 190
+},
+"chara_20":{
+	id: "chara_20",
+	name: "츠무기",
+	icon: "icons/츠무기.png",
+	range: 195
+},
+"chara_21":{
+	id: "chara_21",
+	name: "히요리",
+	icon: "icons/히요리.png",
+	range: 200
+},
+"chara_22":{
+	id: "chara_22",
+	name: "미소기",
+	icon: "icons/미소기.png",
+	range: 205
+},
+"chara_23":{
+	id: "chara_23",
+	name: "아야네",
+	icon: "icons/아야네.png",
+	range: 210
+},
+"chara_24":{
+	id: "chara_24",
+	name: "토모",
+	icon: "icons/토모.png",
+	range: 220
+},
+"chara_25":{
+	id: "chara_25",
+	name: "수영복 타마키",
+	icon: "icons/타마키(수).png",
+	range: 225
+},
+"chara_26":{
+	id: "chara_26",
+	name: "에리코",
+	icon: "icons/에리코.png",
+	range: 230
+},
+"chara_27":{
+	id: "chara_27",
+	name: "수영복 페코린느",
+	icon: "icons/페코린느(수).png",
+	range: 235
+},
+"chara_28":{
+	id: "chara_28",
+	name: "쿠루미",
+	icon: "icons/쿠루미.png",
+	range: 240
+},
+"chara_29":{
+	id: "chara_29",
+	name: "지타",
+	icon: "icons/지타.png",
+	range: 245
+},
+"chara_30":{
+	id: "chara_30",
+	name: "레이",
+	icon: "icons/레이.png",
+	range: 250
+},
+"chara_31":{
+	id: "chara_31",
+	name: "시즈루",
+	icon: "icons/시즈루.png",
+	range: 285
+},
+"chara_32":{
+	id: "chara_32",
+	name: "크리스티나",
+	icon: "icons/크리스티나.png",
+	range: 290
+},
+"chara_33":{
+	id: "chara_33",
+	name: "크리스마스 쿠루미",
+	icon: "icons/쿠루미(성).png",
+	range: 295
+},
+"chara_34":{
+	id: "chara_34",
+	name: "미미",
+	icon: "icons/미미.png",
+	range: 360
+},
+"chara_35":{
+	id: "chara_35",
+	name: "시노부",
+	icon: "icons/시노부.png",
+	range: 365
+},
+"chara_36":{
+	id: "chara_36",
+	name: "발렌타인 시즈루",
+	icon: "icons/시즈루(발).png",
+	range: 385
+},
+"chara_37":{
+	id: "chara_37",
+	name: "마히루",
+	icon: "icons/마히루.png",
+	range: 395
+},
+"chara_38":{
+	id: "chara_38",
+	name: "유카리",
+	icon: "icons/유카리.png",
+	range: 405
+},
+"chara_39":{
+	id: "chara_39",
+	name: "니논",
+	icon: "icons/니논.png",
+	range: 415
+},
+"chara_40":{
+	id: "chara_40",
+	name: "미후유",
+	icon: "icons/미후유.png",
+	range: 420
+},
+"chara_41":{
+	id: "chara_41",
+	name: "이리야",
+	icon: "icons/이리야.png",
+	range: 425
+},
+"chara_42":{
+	id: "chara_42",
+	name: "사렌",
+	icon: "icons/사렌.png",
+	range: 430
+},
+"chara_43":{
+	id: "chara_43",
+	name: "할로윈 시노부",
+	icon: "icons/시노부(할).png",
+	range: 440
+},
+"chara_44":{
+	id: "chara_44",
+	name: "안나",
+	icon: "icons/안나.png",
+	range: 440
+},
+"chara_45":{
+	id: "chara_45",
+	name: "수영복 미후유",
+	icon: "icons/미후유(수).png",
+	range: 495
+},
+"chara_46":{
+	id: "chara_46",
+	name: "콧코로",
+	icon: "icons/콧코로.png",
+	range: 500
+},
+"chara_47":{
+	id: "chara_47",
+	name: "아유미",
+	icon: "icons/아유미.png",
+	range: 510
+},
+"chara_48":{
+	id: "chara_48",
+	name: "수영복 콧코로",
+	icon: "icons/콧코로(수).png",
+	range: 535
+},
+"chara_49":{
+	id: "chara_49",
+	name: "린",
+	icon: "icons/린.png",
+	range: 550
+},
+"chara_50":{
+	id: "chara_50",
+	name: "미츠키",
+	icon: "icons/미츠키.png",
+	range: 565
+},
+"chara_51":{
+	id: "chara_51",
+	name: "아카리",
+	icon: "icons/아카리.png",
+	range: 570
+},
+"chara_52":{
+	id: "chara_52",
+	name: "요리",
+	icon: "icons/요리.png",
+	range: 575
+},
+"chara_53":{
+	id: "chara_53",
+	name: "할로윈 미야코",
+	icon: "icons/미야코(할).png",
+	range: 590
+},
+"chara_54":{
+	id: "chara_54",
+	name: "아리사",
+	icon: "icons/아리사.png",
+	range: 625
+},
+"chara_55":{
+	id: "chara_55",
+	name: "안",
+	icon: "icons/안.png",
+	range: 630
+},
+"chara_56":{
+	id: "chara_56",
+	name: "루",
+	icon: "icons/루.png",
+	range: 640
+},
+"chara_57":{
+	id: "chara_57",
+	name: "리노",
+	icon: "icons/리노.png",
+	range: 700
+},
+"chara_58":{
+	id: "chara_58",
+	name: "스즈나",
+	icon: "icons/스즈나.png",
+	range: 705
+},
+"chara_59":{
+	id: "chara_59",
+	name: "이오",
+	icon: "icons/이오.png",
+	range: 715
+},
+"chara_60":{
+	id: "chara_60",
+	name: "스즈메",
+	icon: "icons/스즈메.png",
+	range: 720
+},
+"chara_61":{
+	id: "chara_61",
+	name: "카스미",
+	icon: "icons/카스미.png",
+	range: 730
+},
+"chara_62":{
+	id: "chara_62",
+	name: "미사토",
+	icon: "icons/미사토.png",
+	range: 735
+},
+"chara_63":{
+	id: "chara_63",
+	name: "나나카",
+	icon: "icons/나나카.png",
+	range: 740
+},
+"chara_64":{
+	id: "chara_64",
+	name: "신년 유이",
+	icon: "icons/유이(신).png",
+	range: 745
+},
+"chara_65":{
+	id: "chara_65",
+	name: "캬루",
+	icon: "icons/캬루.png",
+	range: 750
+},
+"chara_66":{
+	id: "chara_66",
+	name: "하츠네",
+	icon: "icons/하츠네.png",
+	range: 755
+},
+"chara_67":{
+	id: "chara_67",
+	name: "미사키",
+	icon: "icons/미사키.png",
+	range: 760
+},
+"chara_68":{
+	id: "chara_68",
+	name: "크리스마스 치카",
+	icon: "icons/치카(성).png",
+	range: 770
+},
+"chara_69":{
+	id: "chara_69",
+	name: "수영복 스즈메",
+	icon: "icons/스즈메(수).png",
+	range: 775
+},
+"chara_70":{
+	id: "chara_70",
+	name: "수영복 캬루",
+	icon: "icons/캬루(수).png",
+	range: 780
+},
+"chara_71":{
+	id: "chara_71",
+	name: "아오이",
+	icon: "icons/아오이.png",
+	range: 785
+},
+"chara_72":{
+	id: "chara_72",
+	name: "치카",
+	icon: "icons/치카.png",
+	range: 790
+},
+"chara_73":{
+	id: "chara_73",
+	name: "마호",
+	icon: "icons/마호.png",
+	range: 795
+},
+"chara_74":{
+	id: "chara_74",
+	name: "유이",
+	icon: "icons/유이.png",
+	range: 800
+},
+"chara_75":{
+	id: "chara_75",
+	name: "유키",
+	icon: "icons/유키.png",
+	range: 805
+},
+"chara_76":{
+	id: "chara_76",
+	name: "쿄우카",
+	icon: "icons/쿄우카.png",
+	range: 810
+},
+"chara_77":{
+	id: "chara_77",
+	name: "할로윈 미사키",
+	icon: "icons/미사키(할).png",
+	range: 815
+}
+};
